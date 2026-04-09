9f72d8eea64e05afca2b1039bb02043ff2f96d52
wendtalexander <wendtalexander@protonmail.com>
2026-04-09 08:50:19 +0200

    [colorscheme]

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -6,8 +6,8 @@ return {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'rose-pine/neovim',
     name = 'rose-pine',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    -- lazy = tru, -- make sure we load this during startup if it is your main colorscheme
+    priority = 10000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
       require('rose-pine').setup {
