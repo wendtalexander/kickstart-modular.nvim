4461136ac3384983cf4a5f01a170498daca2c96a
wendtalexander <wendtalexander@protonmail.com>
2026-07-05 14:30:34 +0200

    [adding pyrefly]

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -134,6 +134,7 @@ return {
         --     },
         --   },
         -- },
+        pyrefly = {},
         ruff = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
